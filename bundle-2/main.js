3eed1902508e9301cdef997956e8e9304d6c4f9a
nespolisimmaco <snespoli@yahoo.com>
2023-03-27 18:23:05 +0200

    Exercise 2

--- bundle-2/main.js
+++ bundle-2/main.js
@@ -26,31 +26,46 @@ for (let i = 0; i < 5; i++) {
 // 2. l'errore è "i > 5", poiché il loop va da 0 a 4 e quindi "i < 5"
 
 // ESERCIZIO 2
+// SBAGLIATO
+
+// function addIfEven(num) {
+//     if (num % 2 = 0) {
+//         return num + 5;
+//     }
+//     return num;
+// }
+
+// CORRETTO
 function addIfEven(num) {
-    if (num % 2 = 0) {
+    if (num % 2 === 0) {
         return num + 5;
     }
     return num;
 }
+const number = addIfEven(10);
+console.log(number);
+
+// 1. la funzione somma 5 al numero passato come parametro della funzione se questo è pari, e ritorna la somma, altrimenti ritorna il numero
+// 2. nella condizione dell'if deve esserci "===" per verificare l'identità, e non "=" che è assegnazione
+// 3. il numero ritornato dalla funzione va salvato in una variabile e mostrato in console
+
+// // ESERCIZIO 3
+// function loopToFive() {
+//     for (let i = 0, i < 5, i++) {
+//         console.log(i);
+//     }
+// }
 
 
-// ESERCIZIO 3
-function loopToFive() {
-    for (let i = 0, i < 5, i++) {
-        console.log(i);
-    }
-}
-
-
-// ESERCIZIO 4 (suggerimento: ci sono 8 errori)
-function displayEvenNumbers() {
-    let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
-    let evenNumbers = [];
-    for (let i = 0; i < numbers.length - 1; i++;) {
-        if (numbers % 2 = 0); {
-            evenNumbers.push(i);
-        }
-        return evenNumbers;
-    }
-}
-displayEvenNumbers(); // [2,4,6,8]
+// // ESERCIZIO 4 (suggerimento: ci sono 8 errori)
+// function displayEvenNumbers() {
+//     let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+//     let evenNumbers = [];
+//     for (let i = 0; i < numbers.length - 1; i++;) {
+//         if (numbers % 2 = 0); {
+//             evenNumbers.push(i);
+//         }
+//         return evenNumbers;
+//     }
+// }
+// displayEvenNumbers(); // [2,4,6,8]
