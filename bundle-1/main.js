17dd12dc347c8c1d1d816d97f49ba42f989c73d6
nespolisimmaco <snespoli@yahoo.com>
2023-03-27 16:44:49 +0200

    Add: Exercise 2

--- bundle-1/main.js
+++ bundle-1/main.js
@@ -26,34 +26,49 @@
 // checkAge();
 
 // CORRETTO
-const myAge = 32;
-const ageResult = checkAge();
-console.log(ageResult);
+// const myAge = 32;
+// const ageResult = checkAge();
+// console.log(ageResult);
 
-function checkAge() {
-    let message = '';
+// function checkAge() {
+//     let message = '';
 
-    if (myAge < 18) {
-        message = `Sei troppo giovane! Hai ${myAge} anni!`;
-    } else {
-        message = 'Hai più di 18 anni!';
-    }
-    return message;
-}
+//     if (myAge < 18) {
+//         message = `Sei troppo giovane! Hai ${myAge} anni!`;
+//     } else {
+//         message = 'Hai più di 18 anni!';
+//     }
+//     return message;
+// }
 
 // 1. Il codice controlla l'età, se è minore di 18 allora 'Sei minorenne', altrimenti se è maggiore di 18 allora 'sei maggiorenne'
-// 2. (ERRORE 1) la costante "myAge" deve essere dichiarata fuori dalla funzione, altrimenti può essere vista solo all'interno di essa
+// 2. & 3. (ERRORE 1) la costante "myAge" deve essere dichiarata fuori dalla funzione, altrimenti può essere vista solo all'interno di essa
 // (ERRORE 2) message non può essere una costante, altrimenti non è possibile assegnargli una stringa in seguito
 // (ERRORE 3) la funzione deve ritornare la stringa 'message'
 // (ERRORE 4) la stringa che ritorna la funzione va salvata in una variabile, e la varibile va mostrata almeno in console con console.log, altrimenti se si richiama solo la funzione, questa non mostra niente
 
 // // ESERCIZIO 2
+// SBAGLIATO
 // function printColorsNumber() {
 //     const colors = ['blue', 'red', 'yellow', 'green', 'black'];
 //     console.log(`Nella mia palette ci sono ${colors.lenght} colori!`);
 // }
 // printColorsNumber();
 
+// CORRETTO
+const colors = ['blue', 'red', 'yellow', 'green', 'black'];
+const colorsNumber = printColorsNumber();
+console.log(colorsNumber);
+
+function printColorsNumber() {
+    return `Nella mia palette ci sono ${colors.length} colori!`;
+}
+
+// 1. la funzione conta quanti colori ci sono nella palette, ossia nell'array
+// 2. "colors.lenght" è scritto in modo errato, "colors.length" è scritto in modo giusto
+// 3. (ERRORE 1) l'array va dichiarato fuori dalla funzione
+//  (ERRORE 2) nella funzione non deve esserci "console.log", ma "return" che ritorna la stringa
+// (ERRORE 3) la stringa ritornata dalla funzione va salvata in una variabile che poi sarà mostrata con un "console.log"
 
 // // ESERCIZIO 3
 // function addNumbers() {
