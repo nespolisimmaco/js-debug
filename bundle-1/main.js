56d9f904adcec87c49456d00f58a2a3df2a25b5e
nespolisimmaco <snespoli@yahoo.com>
2023-03-27 16:32:21 +0200

    Add: Exercise 1

--- bundle-1/main.js
+++ bundle-1/main.js
@@ -11,57 +11,80 @@
 
 
 // ESERCIZIO 1
+
+// SBAGLIATO
+// function checkAge() {
+//     const myAge = 32;
+//     const message = '';
+
+//     if (myAge < 18) {
+//         message = `Sei troppo giovane! Hai ${myAge} anni!`;
+//     } else {
+//         message = 'Hai più di 18 anni!';
+//     }
+// }
+// checkAge();
+
+// CORRETTO
+const myAge = 32;
+const ageResult = checkAge();
+console.log(ageResult);
+
 function checkAge() {
-    const myAge = 32;
-    const message = '';
+    let message = '';
 
     if (myAge < 18) {
         message = `Sei troppo giovane! Hai ${myAge} anni!`;
     } else {
         message = 'Hai più di 18 anni!';
     }
+    return message;
 }
-checkAge();
+
+// 1. Il codice controlla l'età, se è minore di 18 allora 'Sei minorenne', altrimenti se è maggiore di 18 allora 'sei maggiorenne'
+// 2. (ERRORE 1) la costante "myAge" deve essere dichiarata fuori dalla funzione, altrimenti può essere vista solo all'interno di essa
+// (ERRORE 2) message non può essere una costante, altrimenti non è possibile assegnargli una stringa in seguito
+// (ERRORE 3) la funzione deve ritornare la stringa 'message'
+// (ERRORE 4) la stringa che ritorna la funzione va salvata in una variabile, e la varibile va mostrata almeno in console con console.log, altrimenti se si richiama solo la funzione, questa non mostra niente
+
+// // ESERCIZIO 2
+// function printColorsNumber() {
+//     const colors = ['blue', 'red', 'yellow', 'green', 'black'];
+//     console.log(`Nella mia palette ci sono ${colors.lenght} colori!`);
+// }
+// printColorsNumber();
 
 
-// ESERCIZIO 2
-function printColorsNumber() {
-    const colors = ['blue', 'red', 'yellow', 'green', 'black'];
-    console.log(`Nella mia palette ci sono ${colors.lenght} colori!`);
-}
-printColorsNumber();
+// // ESERCIZIO 3
+// function addNumbers() {
+//     const userNumber = prompt('Inserisci un numero');
+//     const total = userNumber + 12;
+
+//     console.log(`Il risultato finale è ${total}`);
+// }
+// addNumbers();
 
 
-// ESERCIZIO 3
-function addNumbers() {
-    const userNumber = prompt('Inserisci un numero');
-    const total = userNumber + 12;
+// // ESERCIZIO 4
+// function checkAccess() {
+//     const addresses = [
+//         'mymail@mail.com',
+//         'yourmail@mail.com',
+//         'hermail@mail.com',
+//         'hismail@mail.com',
+//     ];
+//     const userEmail = prompt('Inserisci il tuo indirizzo email');
 
-    console.log(`Il risultato finale è ${total}`);
-}
-addNumbers();
+//     let grantAccess = 'false';
 
+//     if (addresses.includes(userEmail)) {
+//         grantAccess = 'true';
+//     }
 
-// ESERCIZIO 4
-function checkAccess() {
-    const addresses = [
-        'mymail@mail.com',
-        'yourmail@mail.com',
-        'hermail@mail.com',
-        'hismail@mail.com',
-    ];
-    const userEmail = prompt('Inserisci il tuo indirizzo email');
-
-    let grantAccess = 'false';
-
-    if (addresses.includes(userEmail)) {
-        grantAccess = 'true';
-    }
-
-    if (grantAccess === true) {
-        console.log('Accesso consentito!');
-    } else {
-        console.log('Accesso negato!');
-    }
-}
-checkAccess();
+//     if (grantAccess === true) {
+//         console.log('Accesso consentito!');
+//     } else {
+//         console.log('Accesso negato!');
+//     }
+// }
+// checkAccess();
